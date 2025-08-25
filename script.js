22dc9181ebfd6bce62985277e41dd09183c9e709
Tinmar <martin.lecerf@hotmail.fr>
2025-08-25 10:41:32 +0200

    nique le CSS très très fort

---
+++ script.js
@@ -0,0 +1,59 @@
+const themeBtn = document.getElementById("theme-btn");
+const descriptionArea = document.getElementById("description");
+const descriptionButton = document.getElementById("description-button");
+const pseudoInput = document.getElementById("pseudo-input");
+const descriptions = ["wesh alors", "keskidi", "bien ou bien"];
+const submitBtn = document.getElementById("submit-btn");
+const image1 = document.getElementById("img-1");
+const image2 = document.getElementById("img-2");
+const image3 = document.getElementById("img-3");
+const images = document.getElementById("avatar-section");
+
+let selectedImage = null;
+let isValid = false;
+pseudoInput.value = "";
+submitBtn.disabled = true;
+let descriptionIndex = 0;
+const updateValidity = () => {
+  isValid = pseudoInput.value !== "" && selectedImage !== null;
+  submitBtn.disabled = !isValid;
+};
+
+const handleSelect = (e) => {
+  document.querySelectorAll("img").forEach((img) => {
+    img.classList.remove("selected");
+  });
+  selectedImage = e.target;
+  e.target.classList.add("selected");
+  updateValidity();
+};
+
+const handleGoNext = () => {
+  document.querySelector("main").innerHTML = `<div class="card">
+    <img src="${selectedImage.src}"> 
+    <p>${pseudoInput.value}</p>
+    <p>${descriptions[descriptionIndex]}</p> 
+    </div>`;
+};
+
+descriptionArea.innerText = descriptions[descriptionIndex];
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+descriptionButton.addEventListener("click", () => {
+  descriptionIndex =
+    descriptionIndex == descriptions.length - 1 ? 0 : descriptionIndex + 1;
+  descriptionArea.innerText = descriptions[descriptionIndex];
+});
+
+document.querySelectorAll("img").forEach((img) => {
+  img.addEventListener("click", handleSelect);
+});
+
+pseudoInput.addEventListener("input", (e) => {
+  updateValidity();
+});
+
+submitBtn.addEventListener("click", handleGoNext);
